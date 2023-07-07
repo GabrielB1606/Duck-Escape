736bf4c6579cb9882c7dfc978e6ef7b7cb327c07
Gabriel Belisario <gabrielbelisariof@gmail.com>
2023-07-07 14:55:50 -0400

    basic webGL setup

--- app.js
+++ app.js
@@ -1,34 +1,24 @@
-var vertexShaderText = 
-[
-'precision mediump float;',
-'',
-'attribute vec2 vertPosition;',
-'attribute vec3 vertColor;',
-'varying vec3 fragColor;',
-'',
-'void main()',
-'{',
-'  fragColor = vertColor;',
-'  gl_Position = vec4(vertPosition, 0.0, 1.0);',
-'}'
-].join('\n');
+const vertexShaderText = `
+  attribute vec2 a_position;
+  
+  void main() {
+    gl_Position = vec4(a_position, 0.0, 1.0);
+  }
+`;
 
-var fragmentShaderText =
-[
-'precision mediump float;',
-'',
-'varying vec3 fragColor;',
-'void main()',
-'{',
-'  gl_FragColor = vec4(fragColor, 1.0);',
-'}'
-].join('\n');
+const fragmentShaderText = `
+  precision mediump float;
+  
+  void main() {
+    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+  }
+`;
 
 const InitDemo = () =>{
     console.log("loading");
 
-    var canvas = document.getElementById("game");
-    var gl = canvas.getContext('webgl');
+    const canvas = document.getElementById("game");
+    const gl = canvas.getContext('webgl');
 
     if(!gl){
         gl = canvas.getContext('experimental-webgl');
@@ -40,5 +30,59 @@ const InitDemo = () =>{
 
     gl.clearColor(0.2, 0.6, 0.2, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+    gl.shaderSource(vertexShader, vertexShaderText);
+    gl.shaderSource(fragmentShader, fragmentShaderText);
+
+    gl.compileShader(vertexShader);
+    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+		console.error('ERROR compiling vertex shader!', gl.getShaderInfoLog(vertexShader));
+		return;
+	}
+
+    gl.compileShader(fragmentShader);
+    if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+		console.error('ERROR compiling fragment shader!', gl.getShaderInfoLog(fragmentShader));
+		return;
+	}
+
+    const shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+		console.error('ERROR linking program!', gl.getProgramInfoLog(shaderProgram));
+		return;
+	}
+
+    gl.validateProgram(shaderProgram);
+	if (!gl.getProgramParameter(shaderProgram, gl.VALIDATE_STATUS)) {
+		console.error('ERROR validating program!', gl.getProgramInfoLog(shaderProgram));
+		return;
+	}
+
+    gl.useProgram(shaderProgram);
+
+    const positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    const vertices = [
+       -1.0,  1.0,
+        1.0,  1.0,
+       -1.0, -1.0,
+        // 1.0, -1.0,
+    ];
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+    const positionAttributeLocation = gl.getAttribLocation(shaderProgram, "a_position");
+    gl.enableVertexAttribArray(positionAttributeLocation);
+    gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+
+    console.log(vertices.length);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, vertices.length/2);
 
 }
